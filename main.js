097d9fcb70ffbd6676af0f05475e2f35988cb653
Luke McCrea <90562614+lukemccrea@users.noreply.github.com>
2022-12-16 19:36:16 +0000

    Added BPS

--- main.js
+++ main.js
@@ -90,7 +90,10 @@ function Step()
     adburger += microwaves*(5+employees/5);
     adburger += factories*(150*employees/20)
     adburger += BurgerFabricator*(1000+(factories*50));
+    BPS = adburger;
     adburger/=60;
     Burgers += adburger;
+
+    document.getElementById('bps').innerHTML = `${BPS} BPS`;
 }
 setInterval(Step, 16);
