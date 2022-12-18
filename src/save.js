be8003b3815a297b0f7c48ff6eead5df7857700d
Large Guy <82416583+Large-Guy@users.noreply.github.com>
2022-12-18 21:37:23 +0000

    Added burger gun to save Your save will not get corrupted when updating to new update.

--- src/save.js
+++ src/save.js
@@ -1,6 +1,6 @@
 function GenerateSave()
 {
-    let str = Burgers+":"+employees+":"+microwaves+":"+factories+":"+bonusBurgers+":"+burgerFabricator+":"+squareBurger;
+    let str = Burgers+":"+employees+":"+microwaves+":"+factories+":"+bonusBurgers+":"+burgerFabricator+":"+squareBurger+":"+burgerGun;
     let encodedstr = "";
     for (let index = 0; index < str.length; index++) {
         let c = str.charCodeAt(index);
@@ -16,12 +16,21 @@ function ImportSave(save)
         encodedstr += String.fromCharCode(c-17);
     }
     splitcode = encodedstr.split(':');
+    if(splitcode[0])
     Burgers = Number(splitcode[0]);
+    if(splitcode[1])
     employees = Number(splitcode[1]);
+    if(splitcode[2])
     microwaves = Number(splitcode[2]);
+    if(splitcode[3])
     factories = Number(splitcode[3]);
+    if(splitcode[4])
     bonusBurgers = Number(splitcode[4]);
+    if(splitcode[5])
     burgerFabricator = Number(splitcode[5]);
+    if(splitcode[6])
     squareBurger = Number(splitcode[6]);
+    if(splitcode[7])
+    burgerGun = Number(splitcode[7]);
     Update_Text();
 }
